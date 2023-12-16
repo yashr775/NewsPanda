197910cc23f1f0d1784159e2f9a06453a10736a8
yashr775 <rajy29629@gmail.com>
2023-12-16 22:22:44 +0530

    some changes made

--- Frontend/src/components/news.tsx
+++ Frontend/src/components/news.tsx
@@ -4,9 +4,9 @@ import Newsitem from "./newsitem";
 
 interface Article {
   title: string;
-  description: string;
+  description: string | null;
   newsurl: string;
-  imageurl: string;
+  imageurl: string | null;
   author: string | null;
   date: string;
   source: string;
@@ -34,18 +34,25 @@ const News = () => {
   }, [articles]);
 
   return (
-    <div>
+    <div className="bg-gray-300 min-h-screen">
       <Navbar />
       {articles.map((article) => {
         return (
-          <div>
+          <div className="mt-10 grid grid-cols-4 gap-4 h-1/4 flex-content-0 ">
+            <p className="z-10">{article.imageurl}</p>
             <Newsitem
               key={article.newsurl}
               newsurl={article.newsurl}
-              title={article.title}
-              imageurl={article.imageurl}
+              title={article.title.slice(0, 45)}
+              imageurl={
+                article.imageurl ? article.imageurl : "No image available"
+              }
               source={article.source}
-              description={article.description}
+              description={
+                article.description
+                  ? article.description.slice(0, 88)
+                  : "No description available"
+              }
               date={article.date}
               author={article.author}
             />
--- Frontend/src/components/newsitem.tsx
+++ Frontend/src/components/newsitem.tsx
@@ -9,7 +9,16 @@ type Props = {
 };
 
 const Newsitem = (props: Props) => {
-  return <div>{props.title}</div>;
+  const { title, description, newsurl, imageurl, author, date, source } = props;
+
+  return (
+    <div className="w-1/2  rounded-lg m-4 p-4 bg-white ">
+      <div>
+        <h1 className="text-xl font-bold">{title}</h1>
+        <img src={imageurl} alt={title} />
+      </div>
+    </div>
+  );
 };
 
 export default Newsitem;
