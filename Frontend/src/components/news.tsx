69c27806ee4a00d759d7d357b1ed9c467ea8153f
yashr775 <rajy29629@gmail.com>
2023-12-20 13:51:25 +0530

    infinite scroll feature implemented

--- Frontend/src/components/news.tsx
+++ Frontend/src/components/news.tsx
@@ -3,6 +3,7 @@ import Navbar from "./navbar";
 import Newsitem from "./newsitem";
 import { useRecoilValue } from "recoil";
 import { newsCategory } from "../atoms";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Source {
   id: string | null;
@@ -22,14 +23,25 @@ interface Article {
 const News = () => {
   const hiddenkey = import.meta.env.VITE_API_KEY;
   const [articles, setArticles] = useState<Article[]>([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(1);
   const category = useRecoilValue(newsCategory);
 
   const updateNews = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apikey=${hiddenkey}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apikey=${hiddenkey}&pagesize=8&page=${page}`;
     const data = await fetch(url);
     const parsedData = await data.json();
 
     const tempArticles = parsedData.articles;
+
+    if (tempArticles.length === 0) {
+      setHasMore(false);
+    } else {
+      setArticles((prevArticles) => [...prevArticles, ...tempArticles]);
+      setPage((prevPage) => prevPage + 1);
+    }
+
+    console.log(parsedData.totalResults);
     setArticles(tempArticles);
   };
 
@@ -45,6 +57,13 @@ const News = () => {
   return (
     <div className="bg-gray-300 min-h-screen">
       <Navbar />
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={updateNews}
+        hasMore={hasMore}
+        loader={<p>Loading...</p>}
+        children={undefined}
+      ></InfiniteScroll>
       <div className="mt-10 flex flex-wrap">
         {articles.map((article) => {
           return (
